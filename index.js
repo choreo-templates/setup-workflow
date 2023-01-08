0fe4a743e219b81d6f6016803db1311d89f6891a
achala dias <achalad@wso2.com>
2023-01-09 01:14:34 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 // const github = require('@actions/github');
-const exec = require("child_process").exec;
+// const exec = require("child_process").exec;
+const exec = require('@actions/exec').exec;
 const path = require('path');
 
 try {
