feea287a2d872f11a3f498070be94b8cf697a67f
achala dias <achalad@wso2.com>
2023-01-10 16:10:20 +0530

    code changes

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const core = require('@actions/core');
 // const exec = require("child_process").exec;
 const exec = require('@actions/exec').exec;
 const path = require('path');
+const appRoot = require('app-root-path');
 
 try {
     // const workflowType = "byoc";
     const workflowType = core.getInput('type', { required: true });
     const envListSecret = core.getInput('envList', { required: true });
     const updatedEnvListSecret = core.getInput('updatedEnvList', { required: true });
-    const scriptFileName = workflowType === "byoc" ? "shell-scripts/byoc.sh" : "shell-scripts/ballerina-workflows.sh";
+    const scriptFileName = workflowType === "byoc" ? "/shell-scripts/byoc.sh" : "/shell-scripts/ballerina-workflows.sh";
 
     let inputList = [envListSecret, updatedEnvListSecret];
     if (workflowType != "byoc") {
         const privateAppToken = core.getInput('privateAppToken', { required: true });
         inputList.push(privateAppToken);
     }
-    const filePath = path.resolve(__dirname, scriptFileName);
+    const filePath = appRoot +  scriptFileName;
     exec(`chmod 0777 ${filePath}`);
     exec(`sh ${filePath}`, inputList);
 
